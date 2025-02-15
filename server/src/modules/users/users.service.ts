d0ac192750cf0eceb2fdaadb198a5fa8df2b09f5
mohrazzak <mohrazzak7@gmail.com>
2025-02-15 11:52:58 +0300

    running but need to fix .env

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { TodosService } from '../todos/todos.service';
 import { InjectKysely } from 'nestjs-kysely';
-import { DB } from 'src/core/types';
+import { DB } from 'src/core/db/types';
 import { Kysely } from 'kysely';
 
 @Injectable()
 export class UsersService {
 
-  constructor(private readonly todosService: TodosService, @InjectKysely() private readonly db: Kysely<DB>) { }
+  constructor(private readonly todosService: TodosService,
+    @InjectKysely() private readonly db: Kysely<DB>) { }
 
   async findAll() {
-    const a = await this.db.selectFrom('category').selectAll().execute();
-    console.log(a.length)
+    return await this.db.selectFrom('user').selectAll().execute();
+
     return 'Hello world';
   }
 
